f21c80a992c68b5fde179006a8b9f5c93f6f6eb2
Khalid Suleymanov <167674232+KhalidSuleymanov@users.noreply.github.com>
2024-04-21 16:44:32 +0400

    Add files via upload

---
+++ EnumDateTime/EnumDateTime/GroupType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTaskEnumDateTime
+{
+    internal enum GroupType:byte
+    {
+        Programming = 1,
+        Design,
+        System,
+    }
+}
